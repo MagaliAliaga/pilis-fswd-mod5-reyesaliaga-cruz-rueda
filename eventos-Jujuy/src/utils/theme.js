675b569b41c2ca8bd9c6712b654f921ca10acf6a
MagaliAliaga <magalyaliaga92@gmail.com>
2023-04-25 16:37:51 -0300

    cambios

---
+++ eventos-Jujuy/src/utils/theme.js
@@ -0,0 +1,18 @@
+export const COLORS = {
+    primary: '#14c3a2' ,
+    secondary: '#0de5a8' ,
+    text: '#263238' ,
+    inactive: '#A0A0A0' ,
+    white: '#FFFFFF' ,
+    grey: '#F1F1F1'
+}
+
+export const SPACING = {
+    xs: 4,
+    sm: 8,
+    md: 16,
+    lg: 24,
+    xl: 40,
+    xxl: 48,
+    xxxl: 64
+}
